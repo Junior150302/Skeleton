80ce34a9774704642fe16c7125a8e8fb88fe1c41
Junior150302 <P2604361@my365.dmu.ac.uk>
2023-04-27 17:50:38 +0100

    Created tests for each property

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Active { get; set; }
+        public int GameNo { get; set; }
+        public string GameName { get; set; }
+        public string GameDescription { get; set; }
+        public byte GamePicture { get; set; }
+        public DateTime DateReleased { get; set; }
+        public double Price { get; set; }
+        public int Available { get; set; }
     }
 }
--- Testing3/tstStock.cs
+++ Testing3/tstStock.cs
@@ -14,7 +14,99 @@ namespace Testing3
             clsStock AGame = new clsStock();
             //test to see that it exists
             Assert.IsNotNull(AGame);
-
         }
+
+        [TestMethod]
+        public void GameNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AGame = new clsStock();
+            //create some test data
+            Int32 TestData = 1;
+            //assign the data to the property
+            AGame.GameNo = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AGame.GameNo, TestData);
+        }
+
+        [TestMethod]
+        public void GameNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AGame = new clsStock();
+            //create some test data
+            string TestData = "Fifa 23";
+            //assign the data to the property
+            AGame.GameName = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AGame.GameName, TestData);
+        }
+
+        [TestMethod]
+        public void GameDecriptionOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AGame = new clsStock();
+            //create some test data
+            string TestData = "Description of game";
+            //assign the data to the property
+            AGame.GameDescription = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AGame.GameDescription, TestData);
+        }
+
+        [TestMethod]
+        public void GamePicturePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AGame = new clsStock();
+            //create some test data
+            //Byte is used as images commonly use that data type
+            Byte TestData = 1;
+            //assign the data to the property
+            AGame.GamePicture = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AGame.GamePicture, TestData);
+        }
+
+        [TestMethod]
+        public void DataReleasedPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AGame = new clsStock();
+            //create some test data
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AGame.DateReleased = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AGame.DateReleased, TestData);
+        }
+
+        [TestMethod]
+        public void PricePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AGame = new clsStock();
+            //create some test data
+            double TestData = 19.99;
+            //assign the data to the property
+            AGame.Price = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AGame.Price, TestData);
+        }
+
+        [TestMethod]
+        public void AvailablePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AGame = new clsStock();
+            //create some test data
+            Int32 TestData = 57;
+            //assign the data to the property
+            AGame.Available = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AGame.Available, TestData);
+        }
+
     }
 }
